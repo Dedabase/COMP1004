ca867ff7d6b6928616aefcfa048e3865eae2a72b
Kevin Deda <91668534+Dedabase@users.noreply.github.com>
2022-05-04 00:47:16 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,90 @@
+localStorage.clear();
+
+function DTFunction()
+            {
+                var x = document.getElementById("DTDIV");
+                var y = document.getElementById("AboutDIV");
+                var z = document.getElementById("AccountDIV");
+                var a = document.getElementById("ContactDIV");
+                if (x.style.display === "none") 
+                {
+                    x.style.display = "block";
+                } 
+                else 
+                {
+                    x.style.display = "none";
+                }
+                if(true)
+                {
+                    y.style.display = "none";
+                    z.style.display = "none";
+                    a.style.display = "none";
+                }
+                
+            }
+function AboutFunction()
+            {
+                var x = document.getElementById("AboutDIV");
+                var y = document.getElementById("DTDIV");
+                var z = document.getElementById("AccountDIV");
+                var a = document.getElementById("ContactDIV");
+                if (x.style.display === "none") 
+                {
+                    x.style.display = "block";
+                } 
+                else 
+                {
+                    x.style.display = "none";
+                }
+                if(true)
+                {
+                    y.style.display = "none";
+                    z.style.display = "none";
+                    a.style.display = "none";
+                }
+                
+            }
+function AccountFunction()
+            {
+                var x = document.getElementById("AccountDIV");
+                var y = document.getElementById("AboutDIV");
+                var z = document.getElementById("DTDIV");
+                var a = document.getElementById("ContactDIV");
+                if (x.style.display === "none") 
+                {
+                    x.style.display = "block";
+                } 
+                else 
+                {
+                    x.style.display = "none";
+                }
+                if(true)
+                {
+                    y.style.display = "none";
+                    z.style.display = "none";
+                    a.style.display = "none";
+                }
+                
+            }
+function ContactFunction()
+            {
+                var x = document.getElementById("ContactDIV");
+                var y = document.getElementById("AboutDIV");
+                var z = document.getElementById("AccountDIV");
+                var a = document.getElementById("DTDIV");
+                if (x.style.display === "none") 
+                {
+                    x.style.display = "block";
+                } 
+                else 
+                {
+                    x.style.display = "none";
+                }
+                 if(true)
+                {
+                    y.style.display = "none";
+                    z.style.display = "none";
+                    a.style.display = "none";
+                }
+                
+            }
